4a4c1bfbd5c2c958b7ecc368beda0250865bf82e
federicocozza <federicocozza@me.com>
2017-02-03 17:17:06 +0100

    not reported and 4th stadium cases removed

--- Convert_genes.R
+++ Convert_genes.R
@@ -43,3 +43,19 @@ row.names(RNAByAliquot) <- RNAByAliquot$genes
 RNAByAliquot <- RNAByAliquot[,-1]
 
 ## Filtering out patients with not reported tumor stage 
+RNAByAliquot <- RNAByAliquot[,-which(RNASeqData$tumor_stage == "not reported")]
+stages <- RNASeqData$tumor_stage[-which(RNASeqData$tumor_stage == "not reported")]
+
+## Calculating index for tumor stages and removing patients at stage 4
+stage4 <- grep("stage iv$", stages, perl = TRUE, value = FALSE)
+RNAByAliquot <- RNAByAliquot[,-stage4]
+stages <- stages[-stage4]
+
+stage1 <- grep("stage i[^iv]*$", stages, perl = TRUE, value = FALSE)
+stage2 <- grep("stage ii[^i]*$", stages, perl = TRUE, value = FALSE)
+stage3 <- grep("stage iii[ab]*$", stages, perl = TRUE, value = FALSE)
+
+## Grouping subclasses for tumor stages
+stages[stage1] <- "stage i"
+stages[stage2] <- "stage ii"
+stages[stage3] <- "stage iii"
