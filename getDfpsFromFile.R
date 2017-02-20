3a5e9f7c149772c9b0882dcf9a9622753da1cc01
MicheleCurci <cortexbox@gmail.com>
2017-02-20 17:02:04 +0100

    - get DFPS results

---
+++ getDfpsFromFile.R
@@ -0,0 +1,29 @@
+getDfpsFromFiles <- function(idx) {
+    res <- list()
+    for(i in idx) {
+        fn <- paste("paramList", i, ".Rdata", sep = "")
+        load(fn)
+        res[[i]] <- list(
+            skipFactor = paramList$skipFactor,
+            zeta = paramList$zeta,
+            piVal = paramList$piVal,
+            overlapping = paramList$overlapping,
+            dfps = paramList$dfps,
+            numGenes = nrow(paramList$dfps)
+        )
+        print(paste(i, "done."))
+    }
+    return(res)
+}
+
+getDfpResults <- function(idx){
+    df <- data.frame()
+    for(i in idx) {
+        fn <- paste("paramList", i, ".Rdata", sep = "")
+        load(fn)
+        df = rbind(df,data.frame(paramList$skipFactor, paramList$zeta, paramList$piVal, paramList$overlapping, nrow(paramList$dfps),fn))
+        print(paste(i, "done."))
+    }
+    colnames(df) <- c("skipFactor","zeta","piVal","overlapping","numGenes","fileName")
+    return(df)
+}
