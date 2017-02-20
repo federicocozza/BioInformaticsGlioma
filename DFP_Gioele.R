a61913ab8ef9ef2abf8ecd5193e8da4668442b8e
federicocozza <federicocozza@me.com>
2017-02-20 11:26:21 +0100

    DFP

---
+++ DFP_Gioele.R
@@ -0,0 +1,179 @@
+library(DFP)
+library(parallel)
+
+#load("RNAFinal.Rdata")
+#load("RNAPatientsFinal.Rdata")
+
+load("RNAByAliquotL.Rdata")
+load("tRNA_final.Rdata")
+
+parDiscretizeExpressionValues <- function(cluster, rmadataset, mfs, zeta = 0.5, overlapping = 2) 
+{
+    rmam <- exprs(rmadataset)
+    rmam[c(1:8), c(1:4)]
+    rmav <- as.vector(pData(phenoData(rmadataset))$class)
+    rmav
+    names(rmav) <- sampleNames(rmadataset)
+    rmav
+    gene.names <- rownames(rmam)
+    gene.names
+    dvs <- NULL
+    #for (ig in gene.names) {
+    #  values <- rmam[ig, ]
+    #  values
+    #  disc.values <- .fuzzyDiscretization(mfs[[ig]]$lel, mfs[[ig]]$mel, 
+    #                                      mfs[[ig]]$hel, values, zeta, overlapping)
+    #  disc.values
+    #  dvs <- rbind(dvs, disc.values)
+    #  dvs
+    #}
+    
+    doit <- function(ig) {
+        #browser()
+        values <- rmam[ig, ]
+        disc.values <- DFP:::.fuzzyDiscretization(mfs[[ig]]$lel, mfs[[ig]]$mel, 
+                                                  mfs[[ig]]$hel, values, zeta, overlapping)
+        #return(c(ig, disc.values))
+        print("done")
+        return(disc.values)
+    }
+    
+    dvs <- t(parSapply(cluster, gene.names, doit))
+    
+    rownames(dvs) <- gene.names
+    head(dvs)
+    attr(dvs, "types") <- rmav
+    dvs
+    return(dvs)
+}
+
+skipOddValues <- function (values, skipFactor = 3) 
+{
+    if (skipFactor > 0) {
+        orderv <- order(values)
+        orderv
+        vals <- values[orderv]
+        vals
+        first <- trunc(length(vals)/4)
+        first
+        third <- trunc(length(vals)/4) * 3
+        third
+        firstValue <- vals[first + 1]
+        firstValue
+        thirdValue <- vals[third + 1]
+        thirdValue
+        RIC <- thirdValue - firstValue
+        RIC
+        lowBarrier <- firstValue - (skipFactor * RIC)
+        lowBarrier
+        highBarrier <- thirdValue + (skipFactor * RIC)
+        highBarrier
+        isOutlier <- values < lowBarrier | values > highBarrier
+        isOutlier
+    } else if (skipFactor == 0) {
+        isOutlier <- rep(FALSE, length(values))
+    }
+    return(isOutlier)
+}
+
+assignInNamespace(".skipOddValues", skipOddValues, "DFP")
+
+getDFP <- function(RNAFinal, RNAPatientsFinal, datasetName, customFileName = NA, restoreFromDvs = NA, skipFactor = 3, zeta = 0.5, piVal = 0.5, overlapping = 1, filterGenes = TRUE, saveData = TRUE, core = 1) {
+    numberOfGenes = nrow(RNAFinal)
+    
+    phenoData <- RNAPatientsFinal
+    names(phenoData)[length(names(phenoData))] <- "class"
+    phenoData <- new("AnnotatedDataFrame",data = phenoData)
+    
+    if (filterGenes) {
+        vars <- apply(RNAFinal, 1, var)
+        RNAReduced <- RNAFinal[order(vars, decreasing = TRUE)[1:numberOfGenes],]
+        esRNA <- ExpressionSet(as.matrix(RNAReduced),phenoData = phenoData)
+    } else {
+        esRNA <- ExpressionSet(as.matrix(RNAFinal),phenoData = phenoData)
+    }
+    
+    mfs <- calculateMembershipFunctions(esRNA, skipFactor); mfs[[1]]
+    if (filterGenes) {
+        toremove <- c()
+        for(i in 1:length(mfs)){
+            if (is.na(mfs[[i]]$lel@center))
+                toremove <- c(toremove, i)
+        }
+        mfs <- mfs[-toremove]
+        RNAReduced <- RNAReduced[-toremove,]
+        esRNA <- ExpressionSet(as.matrix(RNAReduced),phenoData = phenoData)
+    }
+    
+    print(paste("Number of selected genes:", nrow(RNAReduced)))
+    #plotMembershipFunctions(esRNA, mfs, featureNames(esRNA)[1:2])
+    
+    if (class(restoreFromDvs) != "matrix" && is.na(restoreFromDvs)) {
+        if (core > 1) {
+            cl <- makeCluster(core, outfile = "progress.log")
+            dvs <- parDiscretizeExpressionValues(cl, esRNA, mfs, zeta, overlapping); #dvs[1:4,1:10]
+            stopCluster(cl)
+        } else {
+            dvs <- discretizeExpressionValues(esRNA, mfs, zeta, overlapping); #dvs[1:4,1:10]
+        }
+        assign("dvs", dvs, envir = .GlobalEnv)
+        #showDiscreteValues(dvs, featureNames(esRNA)[1:10])
+    } else {
+        dvs <- restoreFromDvs
+    }
+    
+    it <- 0
+    for (val in piVal) {
+        it <- it + 1
+        print(paste("Calculating dfps ", it, " of ", length(piVal), "...", sep = ""))
+        
+        fps <- calculateFuzzyPatterns(esRNA, dvs, val, overlapping); #fps[1:30,]
+        #showFuzzyPatterns(fps, "stage i")[21:50]
+        
+        
+        dfps <- calculateDiscriminantFuzzyPattern(esRNA, fps); #dfps[1:5,]
+        #plotDiscriminantFuzzyPattern(dfps, overlapping)
+        
+        if (saveData) {
+            paramList <- list(
+                skipFactor = skipFactor,
+                zeta = zeta,
+                piVal = val,
+                overlapping = overlapping,
+                filterGenes = filterGenes,
+                numberOfGenes = numberOfGenes,
+                mfs = mfs,
+                dvs = dvs,
+                fps = fps,
+                dfps = dfps,
+                dataset = datasetName
+            )
+            
+            if (is.na(customFileName)) {
+                nFile <- 1
+                while (file.exists(paste("paramList", nFile, ".Rdata", sep = ""))) {
+                    nFile <- nFile + 1
+                }
+                save(paramList, file = paste("paramList", nFile, ".Rdata", sep = ""))
+            } else {
+                save(paramList, file = customFileName)
+            }
+        }
+    }
+    #file.remove("progress.log")
+    
+}
+
+multiDFP <- function(RNAFinal, RNAPatientsFinal, datasetName, skipFactor = 3, zeta = 0.5, piVal = 0.5, overlapping = 1, filterGenes = TRUE, saveData = TRUE, core = 1) {
+    totalCalls <- length(skipFactor) * length(zeta) * length(overlapping)
+    i <- 0
+    for (sf in skipFactor) {
+        for (z in zeta) {
+            for (o in overlapping) {
+                i <- i + 1
+                print(paste("Call ", i, " of ", totalCalls, "...", sep = ""))
+                getDFP(RNAFinal = RNAFinal, RNAPatientsFinal = RNAPatientsFinal, datasetName = datasetName, skipFactor = sf, zeta = z, piVal = piVal, overlapping = o, filterGenes = filterGenes, saveData = saveData, core = core)
+            }
+        }
+    }
+}
---
+++ DFP_Glioma.R
@@ -0,0 +1,34 @@
+load("tRNA_final.RData")
+load("tmiRNA_final.RData")
+load("RNAByAliquotL.RData")
+
+movetolast <- function(data, move) {
+  data[c(setdiff(names(data), move), move)]
+}
+
+movetolast(tRNA_final, 'cases_0_diagnoses_0_tumor_stage')
+
+stage1PatientsRNA <- tRNA_final[tRNA_final$cases_0_diagnoses_0_tumor_stage == "stage i",]
+stage2PatientsRNA <- tRNA_final[tRNA_final$cases_0_diagnoses_0_tumor_stage == "stage ii",]
+stage3PatientsRNA <- tRNA_final[tRNA_final$cases_0_diagnoses_0_tumor_stage == "stage iii",]
+
+## 75% of the sample size
+smp_size_1 <- floor(0.75 * nrow(stage1PatientsRNA))
+smp_size_2 <- floor(0.75 * nrow(stage2PatientsRNA))
+smp_size_3 <- floor(0.75 * nrow(stage3PatientsRNA))
+
+## set the seed to make your partition reproductible
+set.seed(123)
+train_ind_1 <- sample(seq_len(nrow(stage1PatientsRNA)), size = smp_size_1)
+train_ind_2 <- sample(seq_len(nrow(stage2PatientsRNA)), size = smp_size_2)
+train_ind_3 <- sample(seq_len(nrow(stage3PatientsRNA)), size = smp_size_3)
+
+train_1 <- stage1PatientsRNA[train_ind_1, ]
+test_1 <- stage1PatientsRNA[-train_ind_1, ]
+
+train_2 <- stage2PatientsRNA[train_ind_2, ]
+test_2 <- stage2PatientsRNA[-train_ind_2, ]
+
+train_3 <- stage3PatientsRNA[train_ind_3, ]
+test_3 <- stage3PatientsRNA[-train_ind_3, ]
+
