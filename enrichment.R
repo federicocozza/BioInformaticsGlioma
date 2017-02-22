eb0cd6aaf44af036a22ce117866f0b800129a217
MicheleCurci <cortexbox@gmail.com>
2017-02-22 15:13:19 +0100

    - added enrichment

---
+++ enrichment.R
@@ -0,0 +1,27 @@
+library(GSA)
+gmtfile <- system.file("extdata", "msigdb.v5.2.entrez.gmt",package="clusterProfiler")
+c2kegg <- read.gmt(gmtfile)
+
+#sum(gene %in% c2kegg$gene)
+
+egmt <- enricher(gene, TERM2GENE=c2kegg,pvalueCutoff = 0.05)
+#head(egmt)
+
+#View(egmt@result[grep(pattern = "KEGG",x = rownames(egmt@result),ignore.case = F),])
+
+reactome <- egmt@result[grep(pattern = "reactome",x = rownames(egmt@result),ignore.case = T),]
+kegg <- egmt@result[grep(pattern = "kegg",x = rownames(egmt@result),ignore.case = T),]
+biocarta <- egmt@result[grep(pattern = "biocarta",x = rownames(egmt@result),ignore.case = T),]
+
+gmtfile <- system.file("extdata", "c6.all.v5.2.entrez.gmt",package="clusterProfiler")
+c6 <- read.gmt(gmtfile)
+c6 <- unique(c6$ont)
+
+
+reactomeFinal <- reactome[which(reactome$Count >=10),]
+keggFinal <- kegg[which(kegg$Count >=10),]
+c6Final <- egmt@result[egmt@result$ID %in% c6 & egmt@result$Count >= 10,]
+
+save(reactomeFinal,file="reactomeFinal.Rdata")
+save(keggFinal,file="keggFinal.Rdata")
+save(c6Final,file="c6Final.Rdata")
